5dc2fa6ac5ed08dd693e03ae77cb516f22cf541e
lior <audiblemaple@gmail.com>
2023-07-13 18:31:44 +0300

    removed the refresh

--- src/components/login_box.js
+++ src/components/login_box.js
@@ -61,7 +61,6 @@ const Login_box = ({onSwitchScreen, forgot_modal_show, set_forgot_modal_show}) =
                 body: JSON.stringify(payload),
             });
             console.log(response)
-            console.log(response.text())
 
             const data = await response.json();
             if (response.status === 201){
